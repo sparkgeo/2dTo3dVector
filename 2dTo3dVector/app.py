8694bc486cd4cafd81d17a9b729a9973c186112d
Alex Leamy <aleamy@sparkgeo.com>
2022-05-11 15:46:09 -0700

    personalized demo app

--- 2dTo3dVector/app.py
+++ 2dTo3dVector/app.py
@@ -1,6 +1,11 @@
-name = input("What is your name? ")
+name = input("Enter your name: ")
 
-if name == 'Alex':
+if name == 'Alex': 
     print("Hi Alex, Good job!!")
+else:
+    print("Who are you?")
+    
+if name == 'Mike':
+    print("Hi Mike, thanks for reviewing the PR!")
 else:
     print("Who are you??")
