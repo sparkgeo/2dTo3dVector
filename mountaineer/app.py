d9a01f1747f86ee947ea1cddc2223bcf0011be0a
Alex Leamy <aleamy@sparkgeo.com>
2022-05-24 14:22:41 -0700

    renamed directory to mountaineer

---
+++ mountaineer/app.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+
+if name == 'Alex': 
+    print("Hi Alex, Good job!!")
+else:
+    print("Who are you?")
+    
+if name == 'Mike':
+    print("Hi Mike, thanks for reviewing the PR!")
+else:
+    print("Who are you??")
